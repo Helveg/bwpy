9aa2b32225d31cc144221a653e20c0914ee05ebf
Robin De Schepper <robin.deschepper93@gmail.com>
2021-02-03 23:16:36 +0100

    Succesful CI pipelines for docs, tests & coverage (#1)
    * Added gitkeep files to avoid missing `source/_static`
    
    * fixed file extension typo in build workflow
    
    * Added furo as doc requirement
    
    * Installed rar support to 7z on the build runnerr to unpack sample data
    
    * use apt-get
    
    * fixed wget flag
    
    * dropped support for Python 3.6, as latest numpy also dropped them.
    
    * Added coverage package to requirements
    
    * check dest folder
    
    * more debug on file location
    
    * added truncated brw file to use in tests
    
    * fixed tests
    
    Co-authored-by: Robin De Schepper <robin.deschepper93gmail.com>

--- tests/test_file.py
+++ tests/test_file.py
@@ -5,11 +5,11 @@ from helpers import get_sample_path, open_sample, open_sample_copy
 
 class TestFileObjects(unittest.TestCase):
     def test_open_file(self):
-        with bwpy.File(get_sample_path("sample.brw"), "r") as file:
+        with bwpy.File(get_sample_path("truncated_brw.brw"), "r") as file:
             pass
 
 
 class TestFileObjectProperties(unittest.TestCase):
     def test_description(self):
-        with open_sample("sample.brw", "r") as f:
-            print(f.description)
+        with open_sample("truncated_brw.brw", "r") as f:
+            descr = f.description
