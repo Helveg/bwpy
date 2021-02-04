829dcb2ba5945ae520b48dfa00ef67f1f5efb41d
Robin De Schepper <robin.deschepper93gmail.com>
2021-02-04 02:27:23 +0100

    use test helper samples obj

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,52 +1,48 @@
 import unittest
 import numpy
 import bwpy
-from helpers import get_sample_path, open_sample, open_sample_copy
-
-
-brw_sample = "truncated_brw.brw"
-bxr_sample = "truncated_bxr.bxr"
+from helpers import get_sample_path, open_sample, open_sample_copy, samples
 
 
 class TestFileObjects(unittest.TestCase):
     def test_open_file(self):
-        with bwpy.File(get_sample_path(brw_sample), "r") as file:
+        with bwpy.File(get_sample_path(samples.brw), "r") as file:
             pass
 
     def test_brw_type(self):
-        with bwpy.File(get_sample_path(brw_sample), "r") as file:
+        with bwpy.File(get_sample_path(samples.brw), "r") as file:
             self.assertEqual("brw", file.type)
 
     def test_bxr_type(self):
-        with bwpy.File(get_sample_path(bxr_sample), "r") as file:
+        with bwpy.File(get_sample_path(samples.bxr), "r") as file:
             self.assertEqual("bxr", file.type)
 
 
 class TestFileObjectProperties(unittest.TestCase):
     def test_description(self):
-        with open_sample(brw_sample, "r") as f:
+        with open_sample(samples.brw, "r") as f:
             descr = f.description
         self.assertIs(str, type(descr), "Description should be of type str.")
-        with open_sample_copy(brw_sample) as f:
+        with open_sample_copy(samples.brw) as f:
             f.description = "BRW-File Level3 - Hi, I'm Elfo"
             descr = f.description
         self.assertEqual(descr, "BRW-File Level3 - Hi, I'm Elfo")
 
     def test_description_prefix(self):
-        with open_sample_copy(brw_sample) as f:
+        with open_sample_copy(samples.brw) as f:
             with self.assertWarns(UserWarning):
                 f.description = "Hi, I'm Elfo"
             descr = f.description
         self.assertEqual(descr, "BRW-File Level3 - Hi, I'm Elfo")
 
     def test_version(self):
-        with open_sample(brw_sample, "r") as f:
+        with open_sample(samples.brw, "r") as f:
             version = f.version
         self.assertEqual(numpy.int32, type(version))
         self.assertEqual(version, 320)
 
     def test_guid(self):
-        with open_sample(brw_sample, "r") as f:
+        with open_sample(samples.brw, "r") as f:
             guid = f.guid
         self.assertIs(str, type(guid), "GUID should be of type str.")
         self.assertEqual("cfef184e-a0ea-41af-976b-45dcec62d561", guid)
