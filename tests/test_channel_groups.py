a81469e3afd07426a2d0dee430cb5fe6036a1b83
Robin De Schepper <robin.deschepper93gmail.com>
2021-02-04 02:51:33 +0100

    added channel group tests

--- tests/test_channel_groups.py
+++ tests/test_channel_groups.py
@@ -9,3 +9,18 @@ class TestChannelGroups(unittest.TestCase):
         with open_sample(samples.bxr, "r") as f:
             names = f.get_channel_group_names()
             self.assertEqual(["Group 1"], names)
+
+    def test_get_channel_groups(self):
+        with open_sample(samples.bxr, "r") as f:
+            groups = f.get_channel_groups()
+            self.assertEqual("Group 1", groups[0]._name)
+            self.assertEqual(1, len(groups))
+
+    def test_get_channel_group(self):
+        with open_sample(samples.bxr, "r") as f:
+            group = f.get_channel_group(0)
+            self.assertEqual("Group 1", group.name)
+            self.assertRaises(KeyError, f.get_channel_group, 5)
+            group = f.get_channel_group("Group 1")
+            self.assertRaises(KeyError, f.get_channel_group, "Group 2")
+            self.assertEqual(183, len(group.channels))
