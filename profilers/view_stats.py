03bc901b9b70c5335f4f9779b99e6c21c58177d3
Robin De Schepper <robin.deschepper93gmail.com>
2021-02-04 15:29:58 +0100

    show only top 10% of cumulative time functions

--- profilers/view_stats.py
+++ profilers/view_stats.py
@@ -2,4 +2,4 @@ import pstats, os
 from pstats import SortKey
 
 p = pstats.Stats(os.path.join(os.path.dirname(__file__), "stats.p"))
-p.strip_dirs().sort_stats(SortKey.CUMULATIVE).print_stats()
+p.strip_dirs().sort_stats(SortKey.CUMULATIVE).print_stats(0.1)
