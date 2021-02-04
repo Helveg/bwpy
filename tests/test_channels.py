b0a0d62ede464af7732b8baf64f810dcc971a22d
Robin De Schepper <robin.deschepper93gmail.com>
2021-02-04 12:46:51 +0100

    Added channel testing stub

---
+++ tests/test_channels.py
@@ -0,0 +1,9 @@
+import unittest
+import numpy
+import bwpy
+from helpers import get_sample_path, open_sample, open_sample_copy, samples
+
+
+class TestChannels(unittest.TestCase):
+    def test_get_channel(self):
+        pass
